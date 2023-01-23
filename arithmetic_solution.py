189e0f7b7aef98807c06cc98bd060552486699ef
Mark El-Debs <mark.eldebs@hotmail.ca>
2023-01-22 23:06:54 -0500

    working solution

--- arithmetic_solution.py
+++ arithmetic_solution.py
@@ -9,39 +9,48 @@ space = " "
 difference = " "
 lines = ""
 line = "-"
-show_results= True
+show_results= False
 results = []
 results_print = ""
+results_length = []
 
 for x in a:
     split_list.append(x.split())
 
+# Create results list
 n = 0
-if show_results is True:
-    while n < (a.__len__()):
-        if split_list[n][1] == "+":
-            results.append(int(split_list[n][0]) + int(split_list[n][2]))
-        elif split_list[n][1] == "-":
-            results.append(int(split_list[n][0]) - int(split_list[n][2]))
-        else:
-            throw("Invalid argument")
-        n += 1
+#if show_results is True:
+while n < (a.__len__()):
+    if split_list[n][1] == "+":
+        results.append(int(split_list[n][0]) + int(split_list[n][2]))
+    elif split_list[n][1] == "-":
+        results.append(int(split_list[n][0]) - int(split_list[n][2]))
+    else:
+        throw("Invalid argument")
+    n += 1
 
 
+# Create operand strings
 i = 0
 while i < (a.__len__()):
-    print(split_list[i][0])
+    #determine length of both operands
     upper_bound.append(split_list[i][0].__len__())
     lower_bound.append(split_list[i][2].__len__())
+
+    #formating number of spaces by concatanating based on length of operands
     if upper_bound[i] < lower_bound[i]:
         difference = upper_bound[i] - lower_bound[i] - 1
         p += lower_bound[i]*space + split_list[i][0] + space*difference + space*4
         d += split_list[i][1] + space + split_list[i][2] + space*4
         lines += line*(lower_bound[i] + 2) + space*4
+        results_print += space*2 + str(results[i]) + space*4
+
     elif upper_bound[i] == lower_bound[i]:
         p += 2*space + split_list[i][0] + space + space*4
         d += split_list[i][1] + space + split_list[i][2] + space*4
         lines += line*(upper_bound[i]+2) + space*4
+        results_print += space*2 + str(results[i]) + space*4
+
     else:
         difference = upper_bound[i]-lower_bound[i]
         p += split_list[i][0] + space*4
@@ -49,13 +58,36 @@ while i < (a.__len__()):
 
         if difference > 2:
             lines += line*(lower_bound[i]+ difference) + space*4
+            results_print += str(results[i]) + space*4
+
         else:
             lines += line*(lower_bound[i]+ difference + 1) + space*4
+            results_print += space  + str(results[i]) + space*4
+
     i += 1
 
+# Results string cration
+l = 0
 
-    
-print(p)
-print(d)
-print(lines)
-print(results_print)
+'''
+while l < results.__len__():
+    difference = maxupper_bound[l] - lower_bound[l]
+    if difference == 0:
+        results_print += space*2 + str(results[l]) + space*4
+    elif difference >= 3:
+        results_print += str(results[l]) + space*4
+    else:
+        results_print += space*2 + str(results[l]) + space*4
+    print(difference)
+    l += 1
+'''
+
+if show_results == True:
+    print(p)
+    print(d)
+    print(lines)
+    print(results_print)
+else:
+    print(p)
+    print(d)
+    print(lines)
