5b9feb6b8cba38ff51f251b0ee55a2a48909cec9
Mark El-Debs <mark.eldebs@hotmail.ca>
2023-02-01 02:42:06 -0500

    ALLTESTSPASSED

--- arithmetic_2nd_attempt.py
+++ arithmetic_2nd_attempt.py
@@ -1,10 +1,6 @@
-def arithmetic_arranger(problems, results = False):
-
-    #a = ["32 + 698", "9999 + 9", "45 + 43", "1233 - 49", "1 - 12"]
-    #if ((a.__len__() > 0 & a.__len__() <= 5)
+def arithmetic_arranger(problems, results_opt = False):
 
     b = []
-    c = []
     n = 0
     upper_bound = []
     lower_bound = []
@@ -17,15 +13,10 @@ def arithmetic_arranger(problems, results = False):
     third_line = ''
     results_line = ''
     space = ' '
-    return_ans = ''
-    
-
 
     # Condition for 5 arguments
-    if (problems.__len__() > 5):
-        print("Error: Too many arguments.")
-        exit()
-
+    if len(problems) > 5:
+        return("Error: Too many problems.")
 
     for i in problems:
         # Splits function into 2d_array 
@@ -37,13 +28,11 @@ def arithmetic_arranger(problems, results = False):
         
         # Checks for max argument length
         if upper_bound[n] > 4 or lower_bound[n] > 4:
-            print("Error: Numbers cannot be more than four digits.")
-            quit()
+            return("Error: Numbers cannot be more than four digits.")
         
         # Checks if all numbers only contain digits
         if (b[n][0]).isdigit() == False or (b[n][2]).isdigit() == False:
-            print("Error: Numbers must only contain digits.")
-            quit()
+            return("Error: Numbers must only contain digits.")
 
         # Calculates Results while checking input conditions
         if b[n][1] == "+":
@@ -51,15 +40,15 @@ def arithmetic_arranger(problems, results = False):
         elif b[n][1] == "-":
             results.append(int(b[n][0]) - int(b[n][2]))
         else:
-            print("Error: please make sure you are usng addition or subraction.")
-            quit()
+            return("Error: Operator must be '+' or '-'.")
         
         # Adds length of result into array
         results_width.append(str(results[n]).__len__())
-        max_width.append(max(upper_bound[n], lower_bound[n], results_width[n]))
+        max_width.append(max(upper_bound[n], lower_bound[n]))
+        # lower_bound_comp.append(max_width[n])
 
-        if int(max_width[n]) > (int(lower_bound[n])+ 2):
-            lower_bound_comp.append(max_width[n])
+        if (int(upper_bound[n])) > (int(lower_bound[n])):
+            lower_bound_comp.append(max_width[n]+2)
         else:
             lower_bound_comp.append(b[n][2].__len__()+2)
             
@@ -75,15 +64,23 @@ def arithmetic_arranger(problems, results = False):
     for i in range(problems.__len__()):
 
         first_line += space*(lower_bound_comp[i] - upper_bound[i]) + b[i][0] + space*4
-        second_line += b[i][1] + space*(lower_bound_comp[i]-lower_bound[i]- 1) + b[i][2] + space*4
-        third_line += '-'*lower_bound_comp[i] + space*4
+        second_line += b[i][1] + space*(lower_bound_comp[i]-lower_bound[i]-1) + b[i][2] + space*4
+        third_line += '-'*(lower_bound_comp[i]) + space*4
         results_line += space*(lower_bound_comp[i] - results_width[i]) + str(results[i]) + space*4
 
-    if results:
-        results_print = first_line + '\n' + second_line + '\n' + third_line + '\n' + results_line
-        return(results_print)
-    else:
-        results_print = first_line + '\n' + second_line + '\n' + third_line
-        return(results_print)
+    first_line = first_line.rstrip()
+    first_line += "\n"
+    second_line = second_line.rstrip()
+    second_line += "\n"
+    third_line = str(third_line).rstrip()
+    results_line = results_line.rstrip()
 
-print(arithmetic_arranger(["12 + 12"]))
+    if results_opt == True:
+        third_line += "\n"
+        results_print = first_line + second_line + third_line + results_line
+        return (results_print)
+    else:
+        results_print = first_line + second_line + third_line
+        return (results_print)
+
+print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
