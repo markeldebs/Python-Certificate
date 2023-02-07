ed73e558d2d256dedc6b6027e9be5e6c041a9485
Mark El-Debs <mark.eldebs@hotmail.ca>
2023-02-07 13:52:16 -0500

    time finishing touches

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-isdigit
+        if n_days == 2 and day == "Sunday":
+            days_later = "next day"
+            day = "Monday"
+        elif n_days == 2:
+            days_later = "next day"
+            day = "Monday"
--- time_calculator.py
+++ time_calculator.py
@@ -3,6 +3,7 @@ def add_time(start, duration, day = ''):
     start_split = []
 
     week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    week_lower = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
     new_time = start + ' ' + duration + ' ' + day
     new_time.rstrip()
@@ -29,37 +30,88 @@ def add_time(start, duration, day = ''):
     
     new_day_hours = ((total_day_minutes+ total_duration_minutes)/60).__floor__()
     new_day_minutes = (total_day_minutes + total_duration_minutes)%60
+    if new_day_minutes < 10:
+        new_day_minutes = str("0") + str(new_day_minutes)
 
     # Converting back to 12-hour time format
     n_days = 0
     while new_day_hours >= 12:
-
         n_days += 1
         new_day_hours -= 12
-
-
+    
+    if new_day_hours == 0:
+        new_day_hours = 12
+        
+    # print(n_days)
     if n_days%2 == 0:
         new_day_phase = "AM"
     else:
         new_day_phase = "PM"
-
-
+    
+    
     # Determining n days
     # (n_days/2).__floor__()
+    # days_later = ""
+    # day_counter = (n_days/2).floor()
 
-    if day != '':
-        day = week[(week.count(day))+ (n_days/2).__floor__()]
+    days_later = (n_days/2).__floor__()
+    
+    if day != "":
+        week_index = week_lower.index(day.lower())
+        # print(week_index)
+        week_index = int(n_days/2 + week_index)%6
+        day = week[week_index]
+        
+        # Creating print statement
+        if days_later == 0:
+            new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase +" "+ week[week_index]
+        elif days_later == 1:
+            new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase +" "+ week[week_index]+ " (next day)"
+        else:
+            new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase + "+" "+ week[week_index] (" + days_later + " days later)"
+            
+        new_time.rstrip()
+        return(new_time)
+    
+    
+    
+    
+    
+    
+    # if day != "":
+    #     days_later == number_day_print + "days later"
+    #     if n_days == 2 and day == 'Sunday':
+    #         days_later = "next day"
+    #         day = "Monday"
+    #     elif n_days == 2:
+    #         days_later = "next day"
+    #         day = week[week.index(day)+1]
+    #     while (week.index(day)+ int(n_days/2)) > 6:
+    #         if week.index(day) == 6:
+    #             day = "Monday"
+    #             n_days -= 1
+    #         else:
+    #             day = week[int(week.index(day))+int((number_day_print))]
+        
+        
+    
+
+
     
 
     # Creating return string
-    new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase + " " + day
+    if days_later == 0:
+        new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase
+    elif days_later == 1:
+        new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase + " (next day)"
+    else:
+        new_time = str(new_day_hours)+":"+ str(new_day_minutes)+ " " +new_day_phase + " (" + days_later + " days later)"
+        
     new_time.rstrip()
-
-
     return(new_time)
 
 
     # expected = "5:42 PM"
 
-print(add_time("3:30 AM", "22:12", "Monday"))
+print(add_time("10:55 AM", "12:06", "TuesdAy")) 
 
